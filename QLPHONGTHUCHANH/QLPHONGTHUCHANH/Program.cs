e89b34487aabfd0d2ff52cffa9d3494d4765ae6b
Thuy PT <thuyptqnu@gmail.com>
2023-11-06 22:24:18 +0700

    khoi phuc Lop

--- QLPHONGTHUCHANH/QLPHONGTHUCHANH/Program.cs
+++ QLPHONGTHUCHANH/QLPHONGTHUCHANH/Program.cs
@@ -16,7 +16,7 @@ namespace QLPHONGTHUCHANH
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new fKhoLuuTruPhong());
+            Application.Run(new fLogin());
         }
     }
 }
