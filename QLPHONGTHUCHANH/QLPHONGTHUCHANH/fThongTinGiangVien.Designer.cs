9dff9df2b6b0c06aed9cf5cc8258b1f6ab1280b6
bella13062013 <rmahhngok20092002@gmail.com>
2023-11-16 21:31:58 +0700

    Tạo thêm 1 form thông tn giảng viên và hiển thị thông tin giảng viên.

---
+++ QLPHONGTHUCHANH/QLPHONGTHUCHANH/fThongTinGiangVien.Designer.cs
@@ -0,0 +1,209 @@
+﻿namespace QLPHONGTHUCHANH
+{
+    partial class fThongTinGiangVien
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txbEmail = new System.Windows.Forms.TextBox();
+            this.txbSDT = new System.Windows.Forms.TextBox();
+            this.txbKhoa = new System.Windows.Forms.TextBox();
+            this.txbTenGV = new System.Windows.Forms.TextBox();
+            this.txbMaGV = new System.Windows.Forms.TextBox();
+            this.lbEmail = new System.Windows.Forms.Label();
+            this.lbSDT = new System.Windows.Forms.Label();
+            this.lbKhoa = new System.Windows.Forms.Label();
+            this.lbTenGV = new System.Windows.Forms.Label();
+            this.lbMaGV = new System.Windows.Forms.Label();
+            this.lbTieuDeThongTinGV = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txbEmail
+            // 
+            this.txbEmail.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F);
+            this.txbEmail.Location = new System.Drawing.Point(634, 544);
+            this.txbEmail.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txbEmail.Name = "txbEmail";
+            this.txbEmail.ReadOnly = true;
+            this.txbEmail.Size = new System.Drawing.Size(381, 31);
+            this.txbEmail.TabIndex = 31;
+            // 
+            // txbSDT
+            // 
+            this.txbSDT.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F);
+            this.txbSDT.Location = new System.Drawing.Point(634, 458);
+            this.txbSDT.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txbSDT.Name = "txbSDT";
+            this.txbSDT.ReadOnly = true;
+            this.txbSDT.Size = new System.Drawing.Size(381, 31);
+            this.txbSDT.TabIndex = 30;
+            // 
+            // txbKhoa
+            // 
+            this.txbKhoa.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F);
+            this.txbKhoa.Location = new System.Drawing.Point(634, 374);
+            this.txbKhoa.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txbKhoa.Name = "txbKhoa";
+            this.txbKhoa.ReadOnly = true;
+            this.txbKhoa.Size = new System.Drawing.Size(381, 31);
+            this.txbKhoa.TabIndex = 29;
+            // 
+            // txbTenGV
+            // 
+            this.txbTenGV.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F);
+            this.txbTenGV.Location = new System.Drawing.Point(634, 285);
+            this.txbTenGV.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txbTenGV.Name = "txbTenGV";
+            this.txbTenGV.ReadOnly = true;
+            this.txbTenGV.Size = new System.Drawing.Size(381, 31);
+            this.txbTenGV.TabIndex = 28;
+            // 
+            // txbMaGV
+            // 
+            this.txbMaGV.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F);
+            this.txbMaGV.Location = new System.Drawing.Point(634, 202);
+            this.txbMaGV.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txbMaGV.Name = "txbMaGV";
+            this.txbMaGV.ReadOnly = true;
+            this.txbMaGV.Size = new System.Drawing.Size(381, 31);
+            this.txbMaGV.TabIndex = 27;
+            // 
+            // lbEmail
+            // 
+            this.lbEmail.AutoSize = true;
+            this.lbEmail.BackColor = System.Drawing.Color.Transparent;
+            this.lbEmail.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F);
+            this.lbEmail.Location = new System.Drawing.Point(353, 546);
+            this.lbEmail.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbEmail.Name = "lbEmail";
+            this.lbEmail.Size = new System.Drawing.Size(74, 29);
+            this.lbEmail.TabIndex = 26;
+            this.lbEmail.Text = "Email";
+            // 
+            // lbSDT
+            // 
+            this.lbSDT.AutoSize = true;
+            this.lbSDT.BackColor = System.Drawing.Color.Transparent;
+            this.lbSDT.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F);
+            this.lbSDT.Location = new System.Drawing.Point(353, 460);
+            this.lbSDT.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbSDT.Name = "lbSDT";
+            this.lbSDT.Size = new System.Drawing.Size(154, 29);
+            this.lbSDT.TabIndex = 25;
+            this.lbSDT.Text = "Số điện thoại";
+            // 
+            // lbKhoa
+            // 
+            this.lbKhoa.AutoSize = true;
+            this.lbKhoa.BackColor = System.Drawing.Color.Transparent;
+            this.lbKhoa.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F);
+            this.lbKhoa.Location = new System.Drawing.Point(353, 376);
+            this.lbKhoa.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbKhoa.Name = "lbKhoa";
+            this.lbKhoa.Size = new System.Drawing.Size(75, 29);
+            this.lbKhoa.TabIndex = 24;
+            this.lbKhoa.Text = "Khoa ";
+            // 
+            // lbTenGV
+            // 
+            this.lbTenGV.AutoSize = true;
+            this.lbTenGV.BackColor = System.Drawing.Color.Transparent;
+            this.lbTenGV.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F);
+            this.lbTenGV.Location = new System.Drawing.Point(353, 287);
+            this.lbTenGV.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbTenGV.Name = "lbTenGV";
+            this.lbTenGV.Size = new System.Drawing.Size(172, 29);
+            this.lbTenGV.TabIndex = 23;
+            this.lbTenGV.Text = "Tên giảng viên";
+            // 
+            // lbMaGV
+            // 
+            this.lbMaGV.AutoSize = true;
+            this.lbMaGV.BackColor = System.Drawing.Color.Transparent;
+            this.lbMaGV.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F);
+            this.lbMaGV.Location = new System.Drawing.Point(353, 204);
+            this.lbMaGV.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbMaGV.Name = "lbMaGV";
+            this.lbMaGV.Size = new System.Drawing.Size(162, 29);
+            this.lbMaGV.TabIndex = 22;
+            this.lbMaGV.Text = "Mã giảng viên";
+            // 
+            // lbTieuDeThongTinGV
+            // 
+            this.lbTieuDeThongTinGV.AutoSize = true;
+            this.lbTieuDeThongTinGV.BackColor = System.Drawing.Color.Transparent;
+            this.lbTieuDeThongTinGV.Font = new System.Drawing.Font("Microsoft Sans Serif", 33F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbTieuDeThongTinGV.Location = new System.Drawing.Point(479, 80);
+            this.lbTieuDeThongTinGV.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbTieuDeThongTinGV.Name = "lbTieuDeThongTinGV";
+            this.lbTieuDeThongTinGV.Size = new System.Drawing.Size(433, 52);
+            this.lbTieuDeThongTinGV.TabIndex = 21;
+            this.lbTieuDeThongTinGV.Text = "Thông tin giảng viên";
+            // 
+            // fThongTinGiangVien
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::QLPHONGTHUCHANH.Properties.Resources.d46a39dac513124d4b022;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.ClientSize = new System.Drawing.Size(1370, 749);
+            this.Controls.Add(this.txbEmail);
+            this.Controls.Add(this.txbSDT);
+            this.Controls.Add(this.txbKhoa);
+            this.Controls.Add(this.txbTenGV);
+            this.Controls.Add(this.txbMaGV);
+            this.Controls.Add(this.lbEmail);
+            this.Controls.Add(this.lbSDT);
+            this.Controls.Add(this.lbKhoa);
+            this.Controls.Add(this.lbTenGV);
+            this.Controls.Add(this.lbMaGV);
+            this.Controls.Add(this.lbTieuDeThongTinGV);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "fThongTinGiangVien";
+            this.Text = "Thông tin";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.fThongTinGiangVien_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txbEmail;
+        private System.Windows.Forms.TextBox txbSDT;
+        private System.Windows.Forms.TextBox txbKhoa;
+        private System.Windows.Forms.TextBox txbTenGV;
+        private System.Windows.Forms.TextBox txbMaGV;
+        private System.Windows.Forms.Label lbEmail;
+        private System.Windows.Forms.Label lbSDT;
+        private System.Windows.Forms.Label lbKhoa;
+        private System.Windows.Forms.Label lbTenGV;
+        private System.Windows.Forms.Label lbMaGV;
+        private System.Windows.Forms.Label lbTieuDeThongTinGV;
+    }
+}
---
+++ QLPHONGTHUCHANH/QLPHONGTHUCHANH/fThongTinGiangVien.cs
@@ -0,0 +1,41 @@
+﻿using QLPHONGTHUCHANH.DAL;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QLPHONGTHUCHANH
+{
+    public partial class fThongTinGiangVien : Form
+    {
+        public fThongTinGiangVien()
+        {
+            InitializeComponent();
+        }
+
+        private void fThongTinGiangVien_Load(object sender, EventArgs e)
+        {
+            // Sử dụng thông tin đăng nhập đã lưu trữ từ form fLogin
+            string loggedInUsername = fLogin.LoggedInUsername;
+            string loggedInPassword = fLogin.LoggedInPassword;
+
+            // Thực hiện truy vấn cơ sở dữ liệu để lấy thông tin giảng viên dựa trên thông tin đăng nhập
+            TaiKhoanDAL taiKhoanDAL = TaiKhoanDAL.Khoitao;
+            DataTable giangVienInfo = taiKhoanDAL.getGiangVienInfo(loggedInUsername, loggedInPassword);
+            if (giangVienInfo.Rows.Count > 0)
+            {
+                // Hiển thị thông tin giảng viên lên các TextBox trên tabPThongTin
+                txbMaGV.Text = giangVienInfo.Rows[0]["id"].ToString();
+                txbTenGV.Text = giangVienInfo.Rows[0]["tenGiangVien"].ToString();
+                txbKhoa.Text = giangVienInfo.Rows[0]["khoa"].ToString();
+                txbSDT.Text = giangVienInfo.Rows[0]["sdt"].ToString();
+                txbEmail.Text = giangVienInfo.Rows[0]["email"].ToString();
+            }
+        }
+    }
+}
