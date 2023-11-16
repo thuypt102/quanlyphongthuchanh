b4078b6622fa0013cdac6a0e38a4832b79182970
bella13062013 <rmahhngok20092002@gmail.com>
2023-11-16 23:44:16 +0700

    fThongTinQuanLy: Tạo thêm1 form và hiển thị thông tin khi click.

---
+++ QLPHONGTHUCHANH/QLPHONGTHUCHANH/fThongTinQuanLy.Designer.cs
@@ -0,0 +1,204 @@
+﻿namespace QLPHONGTHUCHANH
+{
+    partial class fThongTinQuanLy
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbTieuDeThongTinGV = new System.Windows.Forms.Label();
+            this.lbEmailQL = new System.Windows.Forms.Label();
+            this.lbSDTQL = new System.Windows.Forms.Label();
+            this.lbChucVu = new System.Windows.Forms.Label();
+            this.lbTenQL = new System.Windows.Forms.Label();
+            this.lbMaQL = new System.Windows.Forms.Label();
+            this.txbMaQL = new System.Windows.Forms.TextBox();
+            this.txbTenQL = new System.Windows.Forms.TextBox();
+            this.txbChucVu = new System.Windows.Forms.TextBox();
+            this.txbSDT = new System.Windows.Forms.TextBox();
+            this.txbEmail = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lbTieuDeThongTinGV
+            // 
+            this.lbTieuDeThongTinGV.AutoSize = true;
+            this.lbTieuDeThongTinGV.BackColor = System.Drawing.Color.Transparent;
+            this.lbTieuDeThongTinGV.Font = new System.Drawing.Font("Microsoft Sans Serif", 39.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbTieuDeThongTinGV.Location = new System.Drawing.Point(479, 60);
+            this.lbTieuDeThongTinGV.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbTieuDeThongTinGV.Name = "lbTieuDeThongTinGV";
+            this.lbTieuDeThongTinGV.Size = new System.Drawing.Size(443, 61);
+            this.lbTieuDeThongTinGV.TabIndex = 22;
+            this.lbTieuDeThongTinGV.Text = "Thông tin quản lý";
+            // 
+            // lbEmailQL
+            // 
+            this.lbEmailQL.AutoSize = true;
+            this.lbEmailQL.BackColor = System.Drawing.Color.Transparent;
+            this.lbEmailQL.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbEmailQL.Location = new System.Drawing.Point(381, 507);
+            this.lbEmailQL.Name = "lbEmailQL";
+            this.lbEmailQL.Size = new System.Drawing.Size(81, 31);
+            this.lbEmailQL.TabIndex = 35;
+            this.lbEmailQL.Text = "Email";
+            // 
+            // lbSDTQL
+            // 
+            this.lbSDTQL.AutoSize = true;
+            this.lbSDTQL.BackColor = System.Drawing.Color.Transparent;
+            this.lbSDTQL.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbSDTQL.Location = new System.Drawing.Point(381, 433);
+            this.lbSDTQL.Name = "lbSDTQL";
+            this.lbSDTQL.Size = new System.Drawing.Size(171, 31);
+            this.lbSDTQL.TabIndex = 34;
+            this.lbSDTQL.Text = "Số điện thoại";
+            // 
+            // lbChucVu
+            // 
+            this.lbChucVu.AutoSize = true;
+            this.lbChucVu.BackColor = System.Drawing.Color.Transparent;
+            this.lbChucVu.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbChucVu.Location = new System.Drawing.Point(381, 360);
+            this.lbChucVu.Name = "lbChucVu";
+            this.lbChucVu.Size = new System.Drawing.Size(114, 31);
+            this.lbChucVu.TabIndex = 33;
+            this.lbChucVu.Text = "Chức vụ";
+            // 
+            // lbTenQL
+            // 
+            this.lbTenQL.AutoSize = true;
+            this.lbTenQL.BackColor = System.Drawing.Color.Transparent;
+            this.lbTenQL.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbTenQL.Location = new System.Drawing.Point(381, 283);
+            this.lbTenQL.Name = "lbTenQL";
+            this.lbTenQL.Size = new System.Drawing.Size(155, 31);
+            this.lbTenQL.TabIndex = 32;
+            this.lbTenQL.Text = "Tên quản lý";
+            // 
+            // lbMaQL
+            // 
+            this.lbMaQL.AutoSize = true;
+            this.lbMaQL.BackColor = System.Drawing.Color.Transparent;
+            this.lbMaQL.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbMaQL.Location = new System.Drawing.Point(381, 211);
+            this.lbMaQL.Name = "lbMaQL";
+            this.lbMaQL.Size = new System.Drawing.Size(145, 31);
+            this.lbMaQL.TabIndex = 31;
+            this.lbMaQL.Text = "Mã quản lý";
+            // 
+            // txbMaQL
+            // 
+            this.txbMaQL.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F);
+            this.txbMaQL.Location = new System.Drawing.Point(634, 211);
+            this.txbMaQL.Margin = new System.Windows.Forms.Padding(4);
+            this.txbMaQL.Name = "txbMaQL";
+            this.txbMaQL.ReadOnly = true;
+            this.txbMaQL.Size = new System.Drawing.Size(381, 31);
+            this.txbMaQL.TabIndex = 36;
+            // 
+            // txbTenQL
+            // 
+            this.txbTenQL.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F);
+            this.txbTenQL.Location = new System.Drawing.Point(634, 283);
+            this.txbTenQL.Margin = new System.Windows.Forms.Padding(4);
+            this.txbTenQL.Name = "txbTenQL";
+            this.txbTenQL.ReadOnly = true;
+            this.txbTenQL.Size = new System.Drawing.Size(381, 31);
+            this.txbTenQL.TabIndex = 37;
+            // 
+            // txbChucVu
+            // 
+            this.txbChucVu.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F);
+            this.txbChucVu.Location = new System.Drawing.Point(634, 360);
+            this.txbChucVu.Margin = new System.Windows.Forms.Padding(4);
+            this.txbChucVu.Name = "txbChucVu";
+            this.txbChucVu.ReadOnly = true;
+            this.txbChucVu.Size = new System.Drawing.Size(381, 31);
+            this.txbChucVu.TabIndex = 38;
+            // 
+            // txbSDT
+            // 
+            this.txbSDT.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F);
+            this.txbSDT.Location = new System.Drawing.Point(634, 433);
+            this.txbSDT.Margin = new System.Windows.Forms.Padding(4);
+            this.txbSDT.Name = "txbSDT";
+            this.txbSDT.ReadOnly = true;
+            this.txbSDT.Size = new System.Drawing.Size(381, 31);
+            this.txbSDT.TabIndex = 39;
+            // 
+            // txbEmail
+            // 
+            this.txbEmail.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F);
+            this.txbEmail.Location = new System.Drawing.Point(634, 507);
+            this.txbEmail.Margin = new System.Windows.Forms.Padding(4);
+            this.txbEmail.Name = "txbEmail";
+            this.txbEmail.ReadOnly = true;
+            this.txbEmail.Size = new System.Drawing.Size(381, 31);
+            this.txbEmail.TabIndex = 40;
+            // 
+            // fThongTinQuanLy
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::QLPHONGTHUCHANH.Properties.Resources.d46a39dac513124d4b022;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.ClientSize = new System.Drawing.Size(1370, 749);
+            this.Controls.Add(this.txbEmail);
+            this.Controls.Add(this.txbSDT);
+            this.Controls.Add(this.txbChucVu);
+            this.Controls.Add(this.txbTenQL);
+            this.Controls.Add(this.txbMaQL);
+            this.Controls.Add(this.lbEmailQL);
+            this.Controls.Add(this.lbSDTQL);
+            this.Controls.Add(this.lbChucVu);
+            this.Controls.Add(this.lbTenQL);
+            this.Controls.Add(this.lbMaQL);
+            this.Controls.Add(this.lbTieuDeThongTinGV);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.Name = "fThongTinQuanLy";
+            this.Text = "Thông tin";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.fThongTinQuanLy_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbTieuDeThongTinGV;
+        private System.Windows.Forms.Label lbEmailQL;
+        private System.Windows.Forms.Label lbSDTQL;
+        private System.Windows.Forms.Label lbChucVu;
+        private System.Windows.Forms.Label lbTenQL;
+        private System.Windows.Forms.Label lbMaQL;
+        private System.Windows.Forms.TextBox txbMaQL;
+        private System.Windows.Forms.TextBox txbTenQL;
+        private System.Windows.Forms.TextBox txbChucVu;
+        private System.Windows.Forms.TextBox txbSDT;
+        private System.Windows.Forms.TextBox txbEmail;
+    }
+}
---
+++ QLPHONGTHUCHANH/QLPHONGTHUCHANH/fThongTinQuanLy.cs
@@ -0,0 +1,41 @@
+﻿using QLPHONGTHUCHANH.DAL;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QLPHONGTHUCHANH
+{
+    public partial class fThongTinQuanLy : Form
+    {
+        public fThongTinQuanLy()
+        {
+            InitializeComponent();
+        }
+
+        private void fThongTinQuanLy_Load(object sender, EventArgs e)
+        {
+            //Sử dụng thông tin đăng nhập đã lưu trữ từ form fLogin
+            string loggedInUsername = fLogin.LoggedInUsername;
+            string loggedInPassword = fLogin.LoggedInPassword;
+
+            // Lấy thông tin người quản lý từ cơ sở dữ liệu
+            DataTable quanLyInfo = TaiKhoanDAL.Khoitao.getNguoiQuanLyInfo(loggedInUsername, loggedInPassword);
+
+            if (quanLyInfo.Rows.Count > 0)
+            {
+                // Hiển thị thông tin lên các textbox
+                txbMaQL.Text = quanLyInfo.Rows[0]["id"].ToString();
+                txbTenQL.Text = quanLyInfo.Rows[0]["hoTen"].ToString();
+                txbChucVu.Text = quanLyInfo.Rows[0]["chucVu"].ToString();
+                txbSDT.Text = quanLyInfo.Rows[0]["sdt"].ToString();
+                txbEmail.Text = quanLyInfo.Rows[0]["email"].ToString();
+            }
+        }
+    }
+}
