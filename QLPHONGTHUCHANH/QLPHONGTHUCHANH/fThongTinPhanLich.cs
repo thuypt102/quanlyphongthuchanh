090cfef279359636c9c41dad349e1e9fde0c9b2d
Thuy PT <thuyptqnu@gmail.com>
2023-11-26 16:35:14 +0700

    Update fThongTinPhanLich.cs

--- QLPHONGTHUCHANH/QLPHONGTHUCHANH/fThongTinPhanLich.cs
+++ QLPHONGTHUCHANH/QLPHONGTHUCHANH/fThongTinPhanLich.cs
@@ -25,19 +25,21 @@ namespace QLPHONGTHUCHANH
 
             if (nam.Count > 0)
             {
-                string maxNamHoc = nam[nam.Count - 1];
+                string maxNamHoc = nam.Max(); // Lấy năm học có giá trị lớn nhất
+
+                nam.Clear(); // Xóa tất cả các năm học hiện có
 
                 // Tạo hai item mới từ năm học có giá trị lớn nhất
                 int maxYear = int.Parse(maxNamHoc.Substring(0, 4));
                 string namHoc1 = (maxYear + 1) + "-" + (maxYear + 2);
                 string namHoc2 = (maxYear + 2) + "-" + (maxYear + 3);
 
+                nam.Add(maxNamHoc);
                 nam.Add(namHoc1);
                 nam.Add(namHoc2);
             }
 
             cmbNamHoc.DataSource = nam;
-            cmbNamHoc.DisplayMember = "NamHoc";
         }
     }
 }
